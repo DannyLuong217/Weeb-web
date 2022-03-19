2385dc56c11b6783e012eac8a5e5d3bdfa04e6da
Dang <67477384+DannyLuong217@users.noreply.github.com>
2022-03-19 09:26:54 +0700

    19/3/2022
    Mornign

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,35 +2,32 @@ import NavLogo from '../asset/logo.svg';
 import '../styles/Navbar.css';
 
 function Navbar() {
-    return ( <
-        div className = "Navbar" >
-        <
-        div className = "Navbardetail" >
-        <
-        div className = "Navbar_Logo" >
-        <
-        img src = { NavLogo }
-        alt = "ImageTitle" / >
-        <
-        /div> <
-        div className = "Navbar_Detail" >
-        <
-        div className = "MarginCenterProp" >
-        <
-        div className = "NavbarMenu" >
-        <
-        ul className = "ListItemMenu" >
-        <
-        li className = "ItemMenu" > Về chúng tôi < /li> <
-        li className = "ItemMenu" > Dự án < /li> <
-        li className = "ItemMenu" > Dịch vụ < /li> <
-        /ul> <
-        button className = "ButtonContactNavMenu" > LIÊN HỆ < /button> <
-        /div> <
-        /div> <
-        /div> <
-        /div> <
-        /div>
+    return (
+    <div className="Navbar">
+        <div className="Navbardetail">
+        <div className="Navbar_Logo">
+        <img src={NavLogo} alt="ImageTitle" />
+        </div>
+        <div className="Navbar_Detail">
+            <div className="NavbarMenu">
+            <ul className="ListItemMenu Nar">
+                <li className="ItemMenu">
+                    <a href="" class="">Về chúng tôi</a> 
+                </li>
+                <li className="ItemMenu">
+                    <a href="" class="">Dự án</a>
+                </li>
+                <li className="ItemMenu">
+                    <a href="" class="">Dịch vụ</a>
+                </li>
+            </ul>
+            </div>
+            <div className="ButtonContact_NavMenu">
+                <button>LIÊN HỆ</button>
+            </div>
+        </div>
+        </div>
+    </div>
     );
 }
 
